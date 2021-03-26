8092bd7b6021f72c80207798ca9ab43071d229e9
Atanas Kolev <naskokolev00@gmail.com>
2021-03-26 23:08:56 +0200

    in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,18 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fsof10-1.fna.fbcdn.net/v/t51.2885-19/s320x320/20635165_1942203892713915_5464937638928580608_a.jpg?tp=1&_nc_ht=instagram.fsof10-1.fna.fbcdn.net&_nc_ohc=3nP3ZoUUCXwAX_yL3RA&ccb=7-4&oh=e8b4c64963e022bcac846aaa789c390f&oe=6085CC1C&_nc_sid=7bff83" class="rounded-circle" style="height: 200px;" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="div">
+                <h1>apple</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>774</strong> posts</div>
+                <div class="pr-4"><strong>25.4m</strong> followers</div>
+                <div class="pr-4"><strong>6</strong> following</div>
             </div>
         </div>
     </div>
