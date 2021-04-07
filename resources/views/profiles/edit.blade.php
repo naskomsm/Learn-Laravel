60da21494699e55b1bdde0594310cf43836e324c
Atanas Kolev <naskokolev00@gmail.com>
2021-04-07 18:18:34 +0300

    finished edit profile logic

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <form action="/profile/someID123" enctype="multipart/form-data" method="post">
+    <form action="/profile/{{ $user->profile->id }}" enctype="multipart/form-data" method="post">
         @csrf
         @method('PATCH')
 
