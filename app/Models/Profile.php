83bfec913409844a382e40f09b08c4d645ea44b4
Atanas Kolev <naskokolev00@gmail.com>
2021-04-08 17:26:36 +0300

    Added default image on profile initial creation

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -8,6 +8,12 @@ class Profile extends Model
 {
     protected $guarded = [];
 
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : "profile/cr5GiRbkUaHg2fzzB9VqRjUxFJlyTQ3r7Rys2EVr.png";
+        return "/storage/" . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row"> 
         <div class="col-3 p-5">
-            <img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100">
+            <img src="{{ $user->profile->profileImage() }}" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
@@ -31,7 +31,7 @@
         @foreach ($user->posts as $post)
         <div class="col-4 pb-4">
             <a href="/p/{{ $post->id }}">
-                <img src="/storage/{{ $post-> image }}" class="w-100">
+                <img src="/storage/{{ $post->image }}" class="w-100">
             </a>
         </div>
         @endforeach
